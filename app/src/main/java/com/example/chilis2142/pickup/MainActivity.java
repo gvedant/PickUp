e9c943bff338ce4a4d653fd6a38ca6c797a7ceb5
wangm18 <martin.wang@vanderbilt.edu>
2018-09-02 17:17:03 -0500

    Reload

--- app/src/main/java/com/example/chilis2142/pickup/MainActivity.java
+++ app/src/main/java/com/example/chilis2142/pickup/MainActivity.java
@@ -1,11 +1,6 @@
 package com.example.chilis2142.pickup;
 
 import android.content.Intent;
-<<<<<<< HEAD
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
-=======
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,7 +8,6 @@ import android.os.Bundle;
 import java.util.Arrays;
 import java.util.List;
 
->>>>>>> MartinDev
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -21,27 +15,13 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-<<<<<<< HEAD
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
-=======
->>>>>>> MartinDev
 public class MainActivity extends AppCompatActivity {
     private static final int RC_SIGN_IN = 123;
 
-    private static final int RC_SIGN_IN = 123;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-<<<<<<< HEAD
-
-        loginPage();
-    }
-
-=======
         createSignInIntent();
     }
 
@@ -65,7 +45,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // [START auth_fui_result]
->>>>>>> MartinDev
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -84,27 +63,6 @@ public class MainActivity extends AppCompatActivity {
                 // ...
             }
         }
-<<<<<<< HEAD
-    }
-
-    void loginPage() {
-        // Choose authentication providers
-        List<AuthUI.IdpConfig> providers = Arrays.asList(
-                new AuthUI.IdpConfig.EmailBuilder().build(),
-                new AuthUI.IdpConfig.PhoneBuilder().build(),
-                new AuthUI.IdpConfig.GoogleBuilder().build(),
-                new AuthUI.IdpConfig.FacebookBuilder().build(),
-                new AuthUI.IdpConfig.TwitterBuilder().build());
-
-// Create and launch sign-in intent
-        startActivityForResult(
-                AuthUI.getInstance()
-                        .createSignInIntentBuilder()
-                        .setAvailableProviders(providers)
-                        .build(),
-                RC_SIGN_IN);
-=======
->>>>>>> MartinDev
     }
     // [END auth_fui_result]
 
